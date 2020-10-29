32c11b835cb4c2ea32feafb025a75edd569df439
matthewkingdesign <hello@matthewking.design>
2020-10-29 09:47:14 +0000

    going through functions

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,1 +1,19 @@
-console.log('hello');
+// let
+// let variable declerations can be re assigned in the program. Below we mutated the age variable from 30 to 31.
+// let is block scoped
+let age = 30;
+age = 31;
+console.log(age); // 31
+
+
+// const
+// const variable declerations are declerations that will stay the same. They cannot be mutated. they are immutable.
+
+const birthYear = 1991;
+// birthYear = 1992; // trying to change a const var will return an error
+// const job; // this will also throw an error as you need a value. 
+
+// var
+// dont use it. 
+// it is function scoped. 
+
--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,2 +1,23 @@
 // Activate Strict mode
 'use strict';
+
+// functions
+function logger(){
+    console.log('My name is Matt');
+}
+// invoke / call / run the function
+logger();
+
+function fruitProcessor(apples, oranges){
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+// capture or save the value returned, then log it to the console.
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+// Simply log the value to the console. 
+console.log(fruitProcessor(2, 8));
+
+const appleOrangeJuice = fruitProcessor(5, 4);
+console.log(appleOrangeJuice);
